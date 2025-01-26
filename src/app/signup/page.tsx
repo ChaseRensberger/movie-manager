ecff264a7829aa127beacdd6fb61deae258c5605
ChaseRensberger <chasehrensberger@gmail.com>
2025-01-25 19:11:06 -0500

    update auth ui

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -12,39 +12,43 @@ export default function SignUp() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="flex flex-col gap-8 w-[300px] items-center">
+      <form
+        className="flex flex-col gap-8 w-[300px] items-center"
+        action={action}
+      >
         <h1 className="text-2xl">Sign Up</h1>
-        <form action={action}>
-          <Input placeholder="Email" />
-          {state?.errors?.email && <p>{state.errors.email}</p>}
-          <Input placeholder="Password" />
-          {state?.errors?.password && (
-            <div>
-              <p>Password must:</p>
-              <ul>
-                {state.errors.password.map((error) => (
-                  <li key={error}>- {error}</li>
-                ))}
-              </ul>
-            </div>
-          )}
-          <div className="flex items-center gap-2">
-            <Checkbox id="remember" />
-            <label
-              htmlFor="remember"
-              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-            >
-              Remember me
-            </label>
+        <Input placeholder="Email" />
+        {state?.errors?.email && <p>{state.errors.email}</p>}
+        <Input placeholder="Password" />
+        {state?.errors?.password && (
+          <div>
+            <p>Password must:</p>
+            <ul>
+              {state.errors.password.map((error) => (
+                <li key={error}>- {error}</li>
+              ))}
+            </ul>
           </div>
-          <Button className="w-full" disabled={pending}>
-            Sign Up
-          </Button>
-        </form>
+        )}
+        <div className="flex items-center gap-2">
+          <Checkbox id="remember" />
+          <label
+            htmlFor="remember"
+            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          >
+            Remember me
+          </label>
+        </div>
+        <Button className="w-full" disabled={pending}>
+          Sign Up
+        </Button>
         <p>
-          Already have an account? <Link href="/signin">Sign in</Link>
+          Already have an account?{" "}
+          <Link href="/signin" className="text-blue-500">
+            Sign in
+          </Link>
         </p>
-      </div>
+      </form>
     </main>
   );
 }
